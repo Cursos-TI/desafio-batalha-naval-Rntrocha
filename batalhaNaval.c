9067b6920c50b2000732367c3c1046b4b9a927a0
Rntrocha <renatosousa0706@gmail.com>
2025-10-28 16:20:47 +0000

    COMMIT_INICIANTE

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,37 +4,53 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main()
+{
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("JOGO DE BATALHA NAVAL \n");
+    printf("--------------------\n");
+    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[10][10];
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // iniciar com zero
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    for (int i = 0; i < 10; i++)
+    {
+        for( int j = 0; j<10; j++)
+        {
+            tabuleiro[i][j]=0;
+        }
+    }
+
+    tabuleiro[2][3] = 7;  // COLOCANDO O 7 NA CASA 'D3
+    tabuleiro[2][4] = 7;  // COLOCANDO O 7 NA CASA 'E3'
+    tabuleiro[2][5] = 7;  // COLOCANDO O 7 NA CASA 'F3'
+    tabuleiro[5][7] = 7; // ACRESCENTANDO O 7 NA CASA H6
+    tabuleiro[6][7] = 7; // ACRESCENTANDO O 7 NA CASA H7
+    tabuleiro[7][7] = 7; // ACRESCENTANDO O 7 NA CASA H8
+
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // imprimir colunas
+    for ( int j = 0; j<10; j++)
+        printf(" %c", letras[j]);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("\n");
 
-    return 0;
+    // Imprimir linhas: rótulo 1…10 e os valores da matriz
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", i+ 1);
+        for ( int j = 0; j< 10; j++)
+        {
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+        
+    }
+
+   
+
+    return  0;
 }
+
+
